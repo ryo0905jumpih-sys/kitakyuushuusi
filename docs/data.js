be5ad3c7c02649375d7b5a8069648a810044ceb2
RYO USUKI <ryo0905jump.ih@gmail.com>
2026-01-20 05:50:40 +0900

    Initial data update with fixed logic

--- docs/data.js
+++ docs/data.js
@@ -1,9 +1,9 @@
 window.WEATHER_DATA = {
-  "updated_at": "2026-01-19 05:53",
+  "updated_at": "2026-01-20 05:50",
   "level": 1,
   "result_text": "注意レベル",
   "p3d": 0.0,
-  "p30d": 28.0,
+  "p30d": 25.5,
   "is_dry": false,
   "is_strong_wind": false,
   "notes": "前3日=八幡確定値, 前30日=速報値(八幡)"
