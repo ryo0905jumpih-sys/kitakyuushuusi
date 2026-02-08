5a2998aead2dc0c41511fca49247bedae6bb8d7b
RYO USUKI <ryo0905jump.ih@gmail.com>
2026-02-09 06:02:25 +0900

    Update weather data results

--- docs/data.js
+++ docs/data.js
@@ -1,11 +1,11 @@
 window.WEATHER_DATA = {
-  "updated_at": "2026-02-08 08:46",
+  "updated_at": "2026-02-09 05:58",
   "level": 0,
   "result_text": "該当なし",
   "p3d": 9.0,
   "p30d": 20.5,
-  "is_dry": true,
+  "is_dry": false,
   "is_strong_wind": false,
   "wind_text": "なし",
-  "notes": "前3日=八幡確定値, 前30日=確定値(八幡), 注意報=北九州地方"
+  "notes": "前3日=八幡確定値, 前30日=推定値(八幡), 注意報=北九州地方"
 };
