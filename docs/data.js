2c16796f4ec8e7c27ef8ea678486a95d4340959a
GitHub Action <action@github.com>
2026-05-25 23:36:40 +0000

    Update weather data and screenshot

--- docs/data.js
+++ docs/data.js
@@ -1,11 +1,11 @@
 window.WEATHER_DATA = {
-  "updated_at": "2026-05-25 08:31",
+  "updated_at": "2026-05-26 08:36",
   "level": 0,
   "result_text": "該当なし",
-  "p3d": 0.5,
+  "p3d": 0.0,
   "p30d": 79.5,
   "is_dry": false,
-  "is_strong_wind": false,
-  "wind_text": "なし",
+  "is_strong_wind": true,
+  "wind_text": "あり (響灘)",
   "notes": "前3日=八幡確定値, 前30日=推定値(八幡), 注意報=北九州地方"
 };
