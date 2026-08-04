8c3e34b237663d1eb2152b51a772ae5fda33acd1
GitHub Action <action@github.com>
2026-08-04 23:31:08 +0000

    Update weather data and screenshot

--- docs/data.js
+++ docs/data.js
@@ -1,9 +1,9 @@
 window.WEATHER_DATA = {
-  "updated_at": "2026-08-04 08:33",
+  "updated_at": "2026-08-05 08:30",
   "level": 0,
   "result_text": "該当なし",
   "p3d": 0.0,
-  "p30d": 61.0,
+  "p30d": 35.5,
   "is_dry": false,
   "is_strong_wind": false,
   "wind_text": "なし",
